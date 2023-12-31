d87e627e26068850c01e6dbe2c15dac9f2cd877f
mohamed <mohamed.nafii160@gmail.com>
2023-12-31 18:04:00 +0100

    releasing the header

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Navbar from '@/components/Navbar'
 import Image from 'next/image'
+import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
     <main>
+
 
     </main>
   )
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,6 +4,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { ShoppingBag } from "lucide-react";
+import { Button } from "./ui/button";
 
 
 const links = [
@@ -17,7 +18,7 @@ export default function Navbar() {
   const pathname = usePathname();
   return (
     <header className="mb-8 border-b">
-      <div className="flex items-center justify-between mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl">
+      <div className="flex items-center justify-between mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl ">
         <Link href="/">
           <h1 className="text-2xl md:text-4xl font-bold">
             Shop<span className="text-primary">Wise</span>
@@ -39,20 +40,22 @@ export default function Navbar() {
                   href={link.href}
                   className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-primary"
                 >
-                  {link.name}
+                 {link.name}
                 </Link>
               )}
             </div>
           ))}
         </nav>
-
         <div className="flex divide-x border-r sm:border-l">
-
+        <Button
+            variant={"outline"}
+            className="flex flex-col gap-y-1.5 h-12 w-12 sm:h-20 sm:w-20 md:h-24 md:w-24 rounded-none"
+          >
             <ShoppingBag />
             <span className="hidden text-xs font-semibold text-gray-500 sm:block">
               Cart
             </span>
-
+          </Button>
         </div>
       </div>
     </header>
